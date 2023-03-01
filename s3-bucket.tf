20a119c1a4e03acdc2c8637b131a12e2d0d72a1e
Anurag <anurag.sharma@cloudreach.com>
2023-03-01 17:22:21 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,9 +1,9 @@
+
 module "s3_bucket" {
   source  = "app.terraform.io/test_organization_1122/s3-bucket/aws"
   version = "2.8.0"
-  
-  bucket_prefix = var.prefix
-  bucket = "my-s3-test-bucket"
+  #bucket_prefix = var.prefix
+  bucket = var.prefix + "my-s3-test-bucket"
   acl    = "private"
 
   versioning = {
